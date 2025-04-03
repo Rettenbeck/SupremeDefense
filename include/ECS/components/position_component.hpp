e90234ebb5b62acc97720c04604c8bad78c7d353
Rettenbeck <gottfried.krapf@gmail.com>
2025-04-03 20:15:21 +0200

    initial commit

--- include/ECS/components/position_component.hpp
+++ include/ECS/components/position_component.hpp
@@ -7,7 +7,7 @@ namespace SupDef {
 
     struct PositionComponent : public Component {
         float x = 0.0f, y = 0.0f;
-        float xAbs = 0.0f, yAbs = 0.0f;
+        float xRel = 0.0f, yRel = 0.0f;
 
         PositionComponent(float x_, float y_) : x(x_), y(y_) { addToRegistry(); }
         PositionComponent() { addToRegistry(); }
@@ -28,8 +28,8 @@ namespace SupDef {
         REFLECT_COMPONENT_BEGIN(PositionComponent)
             REFLECT_FIELD(x)
             REFLECT_FIELD(y)
-            REFLECT_FIELD(xAbs)
-            REFLECT_FIELD(yAbs)
+            REFLECT_FIELD(xRel)
+            REFLECT_FIELD(yRel)
         REFLECT_COMPONENT_END()
 
         std::string getTypeName() const override {
