3c791ea56eb823b6d00e6c39617a72956913872b
Rettenbeck <gottfried.krapf@gmail.com>
2025-04-11 14:45:13 +0200

    Initial commit

---
+++ include/Misc/wave.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <ECS/constants.hpp>
+#include <nlohmann/include.hpp>
+
+
+namespace SupDef {
+
+    // DEFINE_SCLASS_BEGIN(Wave)
+    //     ResourceID resourceID = "";
+    //     long amount = 0;
+    //     float rate = 0.0;
+    //     long capacity = INFINITE_CAPACITY;
+    //     bool selfCost = false;
+
+    //     Resource(ResourceID resourceID, long amount = 0, float rate = 0.0, long capacity = 0)
+    //     : resourceID(resourceID), amount(amount), rate(rate), capacity(capacity) {}
+
+    //     REFLECT_COMPONENT_BEGIN(ThisType)
+    //         REFLECT_FIELD(resourceID)
+    //         REFLECT_FIELD(amount)
+    //         REFLECT_FIELD(rate)
+    //         REFLECT_FIELD(capacity)
+    //         REFLECT_FIELD(selfCost)
+    //     REFLECT_COMPONENT_END()
+    // DEFINE_SCLASS_END
+
+    // DEFINE_UNIQUE_AND_LIST(Wave, UWave, Waves);
+
+}
