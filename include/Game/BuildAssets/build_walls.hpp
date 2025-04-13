c31df75765fb0ef1c02e901dd0d47522f5bc777a
Rettenbeck <gottfried.krapf@gmail.com>
2025-04-13 10:48:39 +0200

    Dummy animation for wall removed

--- include/Game/BuildAssets/build_walls.hpp
+++ include/Game/BuildAssets/build_walls.hpp
@@ -11,8 +11,7 @@ namespace SupDef::BuildAssets {
             AS_WALL_WINTER_TILES_CHECK, AS_WALL_WINTER_COL_CHECK, AS_WALL_WINTER_OCCUPY, AS_WALL_WINTER_IMPASSABLE,
             AS_WALL_WINTER_WIDTH, AS_WALL_WINTER_HEIGHT
         );
-        AssetBuilder::addGraphic(asset, "../data/graphics/wall_winter1_ani.png", 1000, true);
-        AssetBuilder::addAnimation(asset, 5, 64, 64, 3);
+        AssetBuilder::addGraphic(asset, "../data/graphics/wall_winter1.png", 1000, true);
     }
 
     void buildWalls(AssetManager* am) {
