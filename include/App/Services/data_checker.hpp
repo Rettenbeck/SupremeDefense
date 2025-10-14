815ce8bd89408c42fd1b9ded23f912e747723b3d
Rettenbeck <gottfried.krapf@gmail.com>
2025-10-14 11:05:24 +0200

    Added checking code

--- include/App/Services/data_checker.hpp
+++ include/App/Services/data_checker.hpp
@@ -10,13 +10,60 @@ namespace SupDef {
 
     class DataChecker {
         private:
-            // json j_replay, j_assets, j_entities, j_collisions;
-            // json j_complete;
-            // GameStarterStatus status = GameStarterStatus::NoGame;
-            // std::string message = "";
+            UAssetManager assetManager;
+            UEntityManager entityManager;
+            UCollisionSystem collisionSystem;
+            UReplay replay;
 
         public:
             DataChecker() { }
+
+            void clear() {
+                assetManager.reset();
+                entityManager.reset();
+                collisionSystem.reset();
+                replay.reset();
+            }
+
+            bool checkAssetManager(json& j) {
+                try {
+                    assetManager->from_json(j);
+                    return true;
+                } catch(const std::exception& e) {
+                    LOG_ERROR(e.what());
+                    return false;
+                }
+            }
+
+            bool checkEntityManager(json& j) {
+                try {
+                    entityManager->from_json(j);
+                    return true;
+                } catch(const std::exception& e) {
+                    LOG_ERROR(e.what());
+                    return false;
+                }
+            }
+
+            bool checkCollisionSystem(json& j) {
+                try {
+                    collisionSystem->from_json(j);
+                    return true;
+                } catch(const std::exception& e) {
+                    LOG_ERROR(e.what());
+                    return false;
+                }
+            }
+
+            bool checkReplay(json& j) {
+                try {
+                    replay->from_json(j);
+                    return true;
+                } catch(const std::exception& e) {
+                    LOG_ERROR(e.what());
+                    return false;
+                }
+            }
 
     };
 
